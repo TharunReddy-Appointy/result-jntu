c4b8f08a1e97a18e5a01e3152b1304a2337df714
Tharunreddy2725 <97734570+Tharunreddy2725@users.noreply.github.com>
2022-06-06 06:39:22 +0530

    Update urls.py

--- telusuko/urls.py
+++ telusuko/urls.py
@@ -23,8 +23,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('',include('calc.urls')),
-    path('admin/', admin.site.urls),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('admin/', admin.site.urls), 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
